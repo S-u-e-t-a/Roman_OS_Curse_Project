3836c33629eb263a5e6387c44da3d7f8f022c731
khlebnikovroman <khlebnikov.roman187@gmail.com>
2021-09-21 00:35:42 +0300

    -изменен интерфейс вывода данных для просмотра состояния страниц -не до конца

--- OS_Curse_Project/MainVM.cs
+++ OS_Curse_Project/MainVM.cs
@@ -6,6 +6,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+using Gu.Wpf.DataGrid2D;
 
 using LiveCharts;
 using LiveCharts.Defaults;
@@ -16,6 +19,8 @@ namespace OS_Curse_Project
     internal class MainVM : INotifyPropertyChanged
     {
         private SeriesCollection _seriesCollection;
+
+        private ObservableCollection<TabItem> _tabs;
         private ObservableCollection<List<char>> answerList;
 
         private CacheReplacementPolicy<char> choosenPolicy;
@@ -97,6 +102,16 @@ namespace OS_Curse_Project
             }
         }
 
+        public ObservableCollection<TabItem> Tabs
+        {
+            get { return _tabs; }
+            set
+            {
+                _tabs = value;
+                OnPropertyChanged();
+            }
+        }
+
         public RelayCommand StartCommand
         {
             get
@@ -104,6 +119,7 @@ namespace OS_Curse_Project
                 return startCommand ??
                        (startCommand = new RelayCommand(o =>
                        {
+                           Tabs = new ObservableCollection<TabItem>(); // окно типа мру фифо и тд
                            SeriesCollection = new SeriesCollection();
                            var basetype = typeof(CacheReplacementPolicy);
                            var childs = Assembly.GetAssembly(basetype).GetTypes()
@@ -111,29 +127,41 @@ namespace OS_Curse_Project
                            pages = InputedPages.ToCharArray().ToList();
                            foreach (var alg in childs)
                            {
+                               var tabsItems =
+                                   new ObservableCollection<
+                                       TabItem>(); // окна типа 1,2,3,4,5 (количество страниц в кэше)
                                var serie = new LineSeries();
                                serie.Title = alg.Name;
                                serie.Values = new ChartValues<ObservablePoint>();
-                               // generic List with no parameters
-
-                               // To create a List<string>
                                Type[] tArgs = {typeof(char)};
                                var target = alg.MakeGenericType(tArgs);
-
-                               // Create an instance - Activator.CreateInstance will call the default constructor.
-                               // This is equivalent to calling new List<string>().
                                for (var i = 1; i < 10; i++)
                                {
+                                   var cachePages = new List<List<char>>();
+                                   cachePages.Add(new List<char>());
                                    var policy = (CacheReplacementPolicy<char>) Activator.CreateInstance(target, i);
                                    foreach (var page in pages)
                                    {
                                        policy.AddPage(page);
+                                       cachePages[cachePages.Count - 1].Add(page);
+                                       foreach (var p in policy.Pages)
+                                       {
+                                           cachePages[cachePages.Count - 1].Add(p);
+                                       }
                                    }
 
                                    serie.Values.Add(new ObservablePoint(i, policy.Interuptions));
+                                   var grid = new DataGrid(); // НАРУШАЕМ ПРИНЦИПЫ MVVM??????????????/
+                                   grid.AutoGenerateColumns = true;
+                                   grid.SetRowsSource(cachePages);
                                }
 
                                SeriesCollection.Add(serie);
+                               Tabs.Add(new TabItem
+                               {
+                                   Header = alg.Name,
+                                   Content = tabsItems
+                               });
                            }
 
                            OnPropertyChanged();
