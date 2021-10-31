424419d5b91dbc26d07a1bbe014e40cbcfc6bbf1
khlebnikovroman <khlebnikov.roman187@gmail.com>
2021-10-31 21:28:40 +0300

    -Доделан второй шанс

--- CacheReplacementPolicies/SecondChance.cs
+++ CacheReplacementPolicies/SecondChance.cs
@@ -1,73 +1,52 @@
-﻿//using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
-//namespace CacheReplacementPolicies
-//{
-//    internal class SecondChance<T> : CacheReplacementPolicy<T>
-//    {
-//        private int _pointer;
-
-//        private readonly List<int> SC;
+namespace CacheReplacementPolicies
+{
+    internal class SecondChance<T> : CacheReplacementPolicy<T>
+    {
+        private readonly List<int> rbits;
 
 
-//        public SecondChance(int countOfPages) : base(countOfPages)
-//        {
-//            SC = new List<int>();
-//            for (var i = 0; i < countOfPages; i++)
-//            {
-//                SC.Add(0);
-//            }
-//        }
+        public SecondChance(int countOfPages) : base(countOfPages)
+        {
+            rbits = new List<int>();
+            for (var i = 0; i < CountOfPages; i++)
+            {
+                rbits.Add(0);
+            }
+        }
 
 
-//        private int pointer
-//        {
-//            get { return _pointer; }
-//            set
-//            {
-//                if (value == CountOfPages)
-//                {
-//                    pointer = 0;
-//                }
-//                else
-//                {
-//                    pointer = value;
-//                }
-//            }
-//        }
-
-
-//        public override void AddPage(T page)
-//        {
-//            if (Pages.Contains(page))
-//            {
-//                SC[Pages.IndexOf(page)] = 1;
-//            }
-//            else
-//            {
-//                Interuptions += 1;
-//                if (Pages.Count < CountOfPages)
-//                {
-//                    Pages.Add(page);
-//                    pointer += 1;
-//                }
-//                else
-//                {
-//                    if (SC.Contains(0))
-//                    {
-//                        if (SC[pointer] == 1)
-//                        {
-//                            pointer += 1;
-//                            AddPage(page);
-//                        }
-//                    }
-//                    else
-//                    {
-//                        Pages.RemoveAt(0);
-//                        Pages.Add(page);
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
-
+        public override void AddPage(T page)
+        {
+            if (Pages.Contains(page))
+            {
+                rbits[Pages.IndexOf(page)] = 1;
+            }
+            else
+            {
+                Interuptions += 1;
+                if (Pages.Count < CountOfPages)
+                {
+                    Pages.Add(page);
+                    rbits[Pages.IndexOf(page)] = 1;
+                }
+                else
+                {
+                    if (rbits[0] == 0)
+                    {
+                        Pages.RemoveAt(0);
+                        Pages.Add(page);
+                    }
+                    else
+                    {
+                        rbits.RemoveAt(0);
+                        rbits.Add(0);
+                        Pages.Add(Pages[0]);
+                        Pages.RemoveAt(0);
+                    }
+                }
+            }
+        }
+    }
+}
